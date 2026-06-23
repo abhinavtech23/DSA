1817f8fd00243ece5cdaf29806eb8e754c54268f
abhinav <abhinavtiwari232003@gmail.com>
2026-06-23 13:57:49 +0530

    new question and concepts

---
+++ leetcode/min_lights_to_illuminate_road.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int minlight(int arr[] , int n= arr.size();){
+    
+    for (int i=0 ; i<n; i++){
+        
+
+    
+
+    
+}
+
+
+
+int main(){
+
+ int arr[10]={1,2,3,4,5,6,7,8,9,12};
+}
